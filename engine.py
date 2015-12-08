60d1e85e58f9c9c783a912427a2473e5d19655e0
alexsyrom <alexsyrom@yandex.ru>
2015-12-08 20:37:04 +0300

    sceleton of shell created

--- engine.py
+++ engine.py
@@ -10,6 +10,7 @@ AUTHOR_NAME = 'Alexey Syromyatnikov'
 class Analyzer(threading.Thread):
     def __init__(self):
         super(Analyzer, self).__init__()
+        self.debug = False
 
     def run(self):
         pass
@@ -20,9 +21,43 @@ class EngineShell(cmd.Cmd):
     prompt = ''
     file = None
 
+    def __init__(self):
+        super(EngineShell, self).__init__()
+        self.analyzer = Analyzer()
+
     def do_uci(self, arg):
         print('id name', ENGINE_NAME)
         print('id author', AUTHOR_NAME)
+        print('uciok')
+
+    def do_debug(self, arg):
+        arg = arg.split()
+        if arg:
+            arg = arg[0]
+        else:
+            return
+        if arg == 'on':
+            self.analyzer.debug = True
+        elif arg == 'off':
+            self.analyzer.debug = False
+
+    def do_isready(self, arg):
+        pass
+
+    def do_setoption(self, arg):
+        pass
+
+    def do_ucinewgame(self, arg):
+        pass
+
+    def do_position(self, arg):
+        pass
+
+    def do_stop(self, arg):
+        pass
+
+    def do_quit(self, arg):
+        pass
 
     def default(self, arg):
         pass
