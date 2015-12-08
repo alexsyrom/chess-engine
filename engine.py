d1bbcf466cdf867e945696124a1896d57d765fb1
alexsyrom <alexsyrom@yandex.ru>
2015-12-08 17:57:25 +0300

    engine.py added

---
+++ engine.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import threading
+
+
+class Analyzer(threading.Thread):
+    def __init__(self):
+        super(Analyzer, self).__init__()
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
