fc5c4ccfb564acfcca1944b4af83fadaa4cc20b8
alexsyrom <alexsyrom@yandex.ru>
2015-12-08 19:40:58 +0300

    EngineShell created

--- engine.py
+++ engine.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import threading
+import cmd
+
+ENGINE_NAME = 'simple UCI chess engine'
+AUTHOR_NAME = 'Alexey Syromyatnikov'
 
 
 class Analyzer(threading.Thread):
@@ -11,5 +15,18 @@ class Analyzer(threading.Thread):
         pass
 
 
+class EngineShell(cmd.Cmd):
+    intro = ''
+    prompt = ''
+    file = None
+
+    def do_uci(self, arg):
+        print('id name', ENGINE_NAME)
+        print('id author', AUTHOR_NAME)
+
+    def default(self, arg):
+        pass
+
+
 if __name__ == '__main__':
-    pass
+    EngineShell().cmdloop()
