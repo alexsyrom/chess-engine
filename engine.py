7cf076f4ccdd4c0dcbe061607a0d03b51799cfe1
alexsyrom <alexsyrom@yandex.ru>
2015-12-09 14:03:15 +0300

    postinit, do_stop

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@
 
 import threading
 import cmd
+import chess
 
 ENGINE_NAME = 'simple UCI chess engine'
 AUTHOR_NAME = 'Alexey Syromyatnikov'
@@ -11,6 +12,17 @@ class Analyzer(threading.Thread):
     def __init__(self):
         super(Analyzer, self).__init__()
         self.debug = False
+        self.is_working = threading.Event()
+        self.is_working.clear()
+        self.is_conscious = threading.Condition()
+        self.is_bestmove_ready = threading.Event()
+        self.is_bestmove_ready.clear()
+
+        self._bestmove = chess.Move.null()
+
+    @property
+    def bestmove(self):
+        return self._bestmove.uci()
 
     def run(self):
         pass
@@ -23,7 +35,11 @@ class EngineShell(cmd.Cmd):
 
     def __init__(self):
         super(EngineShell, self).__init__()
+        self.postinitialized = False
+
+    def postinit(self):
         self.analyzer = Analyzer()
+        self.postinitialized = True
 
     def do_uci(self, arg):
         print('id name', ENGINE_NAME)
@@ -42,7 +58,12 @@ class EngineShell(cmd.Cmd):
             self.analyzer.debug = False
 
     def do_isready(self, arg):
-        pass
+        if not self.postinitialized:
+            self.postinit()
+        if self.analyzer.is_working.is_set():
+            with self.analyzer.is_conscious:
+                self.analyzer.is_conscious.wait()
+        print('readyok')
 
     def do_setoption(self, arg):
         pass
@@ -53,8 +74,15 @@ class EngineShell(cmd.Cmd):
     def do_position(self, arg):
         pass
 
-    def do_stop(self, arg):
+    def do_go(self, arg):
         pass
+
+    def do_stop(self, arg):
+        if self.analyzer.is_working.is_set():
+            self.analyzer.is_working.clear()
+            with self.analyzer.is_bestmove_ready:
+                self.analyzer.is_bestmove_ready.wait()
+        print(self.analyzer.bestmove)
 
     def do_quit(self, arg):
         pass
